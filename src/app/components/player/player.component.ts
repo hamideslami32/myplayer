1a6da3d5c5da6d6de7a436fc34f20c8cd7ca136e
hamid eslami <heslami32@gmail.com>
2020-05-23 03:35:35 +0430

    feature - add like & dislike

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -9,11 +9,16 @@ export class PlayerComponent implements OnInit {
 
   videoUrl = 'https://tmod.ir/static/Ready%20player%20web_%20Building%20modern%20web%20games%20_Chrome%20Dev%20Summit%202019_.mp4';
   title = 'Ready player web_ Building modern web games (Chrome Dev Summit 2019)';
+  likeStatus = ''
   
   constructor() { 
   }
 
   ngOnInit(): void {
+  }
+
+  setLike(value: string) {
+    this.likeStatus = value
   }
 
 }
